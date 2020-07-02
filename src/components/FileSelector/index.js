db703e161ab13077dfc8dc5cf80793f27b30989a
maksim.valynets <maksim.valynets@bloc-x.com>
2020-07-02 19:21:23 +0200

    Added slider for mobile

--- src/components/FileSelector/index.js
+++ src/components/FileSelector/index.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
 import ReactPlayer from 'react-player';
 import axios from 'axios';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import MagicSliderDots from 'react-magic-slider-dots';
+import 'react-magic-slider-dots/dist/magic-dots.css';
 import './index.css';
 
 export default class FileSelector extends Component {
@@ -23,28 +28,56 @@ export default class FileSelector extends Component {
   }
 
   render() {
+    const settings = {
+      dots: true,
+      arrows: false,
+      infinite: false,
+      speed: 500,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      appendDots: dots => {
+        return <MagicSliderDots dots={dots} numDotsToShow={4} dotWidth={30} />;
+      }
+    };
     return (
       <div className='selectorContainer'>
-        <div className='leftPanel'>
-          { this.state.files.map(file => 
-            (
-              <div key={file.url} className='previewPlayer'>
-                {file.type === 'video' ? (
-                  <ReactPlayer width='100%' height='100%' url={file.url} />
-                ) : (
-                  <img alt='' src={file.url} />
-                )}
-                <div className='clickableArea' onClick={() => this.showFile(file)}></div>
-              </div>
-            )
-          )}
+        <div className='desctopView'>
+          <div className='leftPanel'>
+            { this.state.files.map(file => 
+              (
+                <div key={file.url} className='previewPlayer'>
+                  {file.type === 'video' ? (
+                    <ReactPlayer width='100%' height='100%' url={file.url} />
+                  ) : (
+                    <img alt='' src={file.url} />
+                  )}
+                  <div className='clickableArea' onClick={() => this.showFile(file)}></div>
+                </div>
+              )
+            )}
+          </div>
+          <div className='fileView'>
+            {this.state.activeFile.type === 'video' ? (
+              <ReactPlayer width='100%' height='100%' url={this.state.activeFile.url} />
+            ) : (
+              <img alt='' src={this.state.activeFile.url} />
+            )}
+          </div>
         </div>
-        <div className='fileView'>
-          {this.state.activeFile.type === 'video' ? (
-            <ReactPlayer width='100%' height='100%' url={this.state.activeFile.url} />
-          ) : (
-            <img alt='' src={this.state.activeFile.url} />
-          )}
+        <div className='mobileView'>
+          <Slider {...settings}>
+            { this.state.files.map(file => 
+              (
+                <div key={file.url} className='previewPlayer'>
+                  {file.type === 'video' ? (
+                    <ReactPlayer width='100%' height='100%' url={file.url} />
+                  ) : (
+                    <img alt='' src={file.url} />
+                  )}
+                </div>
+              )
+            )}
+          </Slider>
         </div>
       </div>
     )
